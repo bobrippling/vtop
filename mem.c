0110074d49e1abdd547fbe9eaa79a4f7a1cd5325
Rob Pilling <rob@rpi>
2016-02-06 13:30:05 +0000

    Memory handling routines

---
+++ mem.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <errno.h>
+
+#include "mem.h"
+
+static noreturn
+void alloc_die(size_t l)
+{
+	fprintf(stderr, "couldn't allocate %zu bytes: %s\n",
+			l, strerror(errno));
+	exit(2);
+}
+
+void *xmalloc(size_t l)
+{
+	void *p = malloc(l);
+
+	if(!p)
+		alloc_die(l);
+
+	return p;
+}
+
+void *xrealloc(void *p, size_t l)
+{
+	void *r = realloc(p, l);
+
+	if(!r)
+		alloc_die(l);
+
+	return r;
+}
+
+char *xstrdup(const char *s)
+{
+	size_t l = strlen(s) + 1;
+	char *new = xmalloc(l);
+
+	memcpy(new, s, l);
+
+	return new;
+}
---
+++ mem.h
@@ -0,0 +1,12 @@
+#ifndef MEM_H
+#define MEM_H
+
+#include <stddef.h>
+
+void *xmalloc(size_t);
+void *xrealloc(void *, size_t);
+char *xstrdup(const char *);
+
+#define xnew(T) (T *)xmalloc(sizeof(T))
+
+#endif
